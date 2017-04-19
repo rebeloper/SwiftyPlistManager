5c7ffb3bff8cc4518f95a419d7562c58e8113c8e
Rebeloper <Rebeloper@SANDORs-MacBook-Pro-5.local>
2017-04-19 19:13:50 +0300

    Initial Commit

---
+++ SwiftyPlistManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyPlistManager
+//
+//  Created by Alex Nagy on 19/04/2017.
+//  Copyright © 2017 Rebeloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+

f53dc4b4a9a5bb3f1cd06e442ede2aca302e97a8
Rebeloper <Rebeloper@192-168-0-196.rdsnet.ro>
2017-04-22 20:02:46 +0300

    fixed indentation

--- SwiftyPlistManager/ViewController.swift
+++ SwiftyPlistManager/ViewController.swift
@@ -138,11 +138,11 @@ class ViewController: UIViewController {
     // After adding the call Build and Run your app again.
     
     /*
-     SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
-     if err == nil {
-     print("-------------------> The Value for Key '\(newKey)' actually exists in the '\(dataPlistName).plist' file. It is: '\(result ?? "No Value Fetched")'")
-     }
-     }*/
+    SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
+      if err == nil {
+        print("-------------------> The Value for Key '\(newKey)' actually exists in the '\(dataPlistName).plist' file. It is: '\(result ?? "No Value Fetched")'")
+      }
+    }*/
     
     // Hurray! Comment back that call.
     
@@ -151,11 +151,11 @@ class ViewController: UIViewController {
     // so we are going to do it in code.
     
     /*
-     SwiftyPlistManager.shared.save(helloNewValue, forKey: newKey, toPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("------------------->  Value '\(helloNewValue)' successfully saved at Key '\(newKey)' into '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.save(helloNewValue, forKey: newKey, toPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("------------------->  Value '\(helloNewValue)' successfully saved at Key '\(newKey)' into '\(dataPlistName).plist'")
+      }
+    }*/
     
     // Build & Run. Stop.
     // Note that you don't see any changes in the Data.plist file. This is how it should be, because the app
@@ -163,11 +163,11 @@ class ViewController: UIViewController {
     // value. Comment out the call below.
     
     /*
-     SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
-     if err == nil {
-     print("-------------> The Value for Key '\(newKey)' actually exists. It is: '\(result!)'")
-     }
-     }*/
+    SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
+      if err == nil {
+        print("-------------> The Value for Key '\(newKey)' actually exists. It is: '\(result!)'")
+      }
+    }*/
     
     // Build & Run and take a look at the Log. Stop. Comment back the lines from above.
     // Note that the value you get back is an optional. Retrieveng it with the 'bang operator' (!) is quite risky
@@ -176,12 +176,11 @@ class ViewController: UIViewController {
     // There are better ways to write code. For a start let's add a default value.
     
     /*
-     SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
-     if err == nil {
-     print("-------------> The Value for Key '\(newKey)' actually exists. It is: '\(result ?? "No Value")'")
-     }
-     }*/
-    
+    SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
+      if err == nil {
+        print("-------------> The Value for Key '\(newKey)' actually exists. It is: '\(result ?? "No Value")'")
+      }
+    }*/
     
     // Build & Run and take a look at the Log. Stop. Comment back the lines from above.
     // At this point the optional value will default to the "No Value" Sting. I personally hate working with default
@@ -189,32 +188,32 @@ class ViewController: UIViewController {
     // unwrap the 'result' with a guard-let statement.
     
     /*
-     SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
-     if err == nil {
-     guard let result = result else {
-     print("-------------> The Value for Key '\(newKey)' does not exists.")
-     return
-     }
-     print("-------------> The Value for Key '\(newKey)' actually exists. It is: '\(result)'")
-     }
-     }*/
+    SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
+      if err == nil {
+        guard let result = result else {
+          print("-------------> The Value for Key '\(newKey)' does not exists.")
+          return
+        }
+        print("-------------> The Value for Key '\(newKey)' actually exists. It is: '\(result)'")
+      }
+    }*/
     
     // Or you can unwrap it with an if-let statement if you do not wish to return from the completion handler right away
     
     /*
-     SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
-     if err == nil {
-     
-     if let result = result {
-     print("-------------> The Value for Key '\(newKey)' actually exists. It is: '\(result)'")
-     } else {
-     print("-------------> The Value for Key '\(newKey)' does not exists.")
-     }
-     
-     print("-------------> This line will be printed out regardless if the Value for Key '\(newKey)' exists or not.")
-     
-     }
-     }*/
+    SwiftyPlistManager.shared.getValue(for: newKey, fromPlistWithName: dataPlistName) { (result, err) in
+      if err == nil {
+        
+        if let result = result {
+          print("-------------> The Value for Key '\(newKey)' actually exists. It is: '\(result)'")
+        } else {
+          print("-------------> The Value for Key '\(newKey)' does not exists.")
+        }
+        
+        print("-------------> This line will be printed out regardless if the Value for Key '\(newKey)' exists or not.")
+        
+      }
+    }*/
     
     // Congratulations! You have learned how and when to use (or not to use) the 'bang operator', 'guard-let statements'
     // and 'if-let' statements. You now have solid knowledge of how to deal with optionals.
@@ -224,30 +223,30 @@ class ViewController: UIViewController {
     // unwrap it with a guard-let statement.
     
     /*
-     guard let fetchedValue = SwiftyPlistManager.shared.fetchValue(for: newKey, fromPlistWithName: dataPlistName) else { return }
-     print("-------------> The Fetched Value for Key '\(newKey)' actually exists. It is: '\(fetchedValue)'")*/
+    guard let fetchedValue = SwiftyPlistManager.shared.fetchValue(for: newKey, fromPlistWithName: dataPlistName) else { return }
+    print("-------------> The Fetched Value for Key '\(newKey)' actually exists. It is: '\(fetchedValue)'")*/
     
     ////////////////////////////////////////////////////////////////////////////////////
     // Of course if you try to fetch a value with a non-existent key, 'SwiftyPlistManager' has your back.
     // It will show a WARNING in the log that the key does not exist AND it will not crash the app. Sweet!
-    
+
     /*
-     guard let nonExistentValue = SwiftyPlistManager.shared.fetchValue(for: nonExistentKey, fromPlistWithName: dataPlistName) else {
-     print("-------------> The Value does not exist so going to return now!")
-     return
-     }
-     print("-------------> This line will never be executed because the Key is: '\(nonExistentKey)' so the Value is '\(nonExistentValue)'")*/
+    guard let nonExistentValue = SwiftyPlistManager.shared.fetchValue(for: nonExistentKey, fromPlistWithName: dataPlistName) else {
+      print("-------------> The Value does not exist so going to return now!")
+      return
+    }
+    print("-------------> This line will never be executed because the Key is: '\(nonExistentKey)' so the Value is '\(nonExistentValue)'")*/
     
     ////////////////////////////////////////////////////////////////////////////////////
     // Now let's take a look at some other awesome powers that come with SwiftyPlistManager.
     // You can add a new key-value pair like so:
     
     /*
-     SwiftyPlistManager.shared.addNew(rebeloperValue, key: firstKey, toPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Value '\(rebeloperValue)' successfully added at Key '\(firstKey)' into '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.addNew(rebeloperValue, key: firstKey, toPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Value '\(rebeloperValue)' successfully added at Key '\(firstKey)' into '\(dataPlistName).plist'")
+      }
+    }*/
     
     // Now Build & Run your project... Congratulations! You have just created and saved your first
     // key-value pair into your plist file. Stop the project from running.
@@ -256,11 +255,11 @@ class ViewController: UIViewController {
     // Now add a next value with an Int
     
     /*
-     SwiftyPlistManager.shared.addNew(intValue, key: secondKey, toPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Value '\(intValue)' successfully added at Key '\(secondKey)' into '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.addNew(intValue, key: secondKey, toPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Value '\(intValue)' successfully added at Key '\(secondKey)' into '\(dataPlistName).plist'")
+      }
+    }*/
     
     // Build  & Run again. Take a look at your progress in the Log. Stop.
     
@@ -268,11 +267,11 @@ class ViewController: UIViewController {
     // And finally add an item that has a Bool value
     
     /*
-     SwiftyPlistManager.shared.addNew(boolValue, key: thirdKey, toPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Value '\(boolValue)' successfully added at Key '\(thirdKey)' into '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.addNew(boolValue, key: thirdKey, toPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Value '\(boolValue)' successfully added at Key '\(thirdKey)' into '\(dataPlistName).plist'")
+      }
+    }*/
     
     // Build & Run again. Take a look at your progress in the Log. Notice that 'true' is represented
     // with a '1' and 'false' is represented with a '0'. Stop your project from running.
@@ -295,11 +294,11 @@ class ViewController: UIViewController {
     // Change and at the same time Save the second key's value to '28' (anotherIntValue)
     
     /*
-     SwiftyPlistManager.shared.save(anotherIntValue, forKey: secondKey, toPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Value '\(anotherIntValue)' successfully saved at Key '\(secondKey)' into '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.save(anotherIntValue, forKey: secondKey, toPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Value '\(anotherIntValue)' successfully saved at Key '\(secondKey)' into '\(dataPlistName).plist'")
+      }
+    }*/
     
     // Of course, Build & Run again. And after you take a look at your progress. Stop.
     // And comment back the line. From now on do it on every new task. :)
@@ -312,21 +311,21 @@ class ViewController: UIViewController {
     // make the save anyway. It is your responsibility that you save the right types of values. Try it out.
     
     /*
-     SwiftyPlistManager.shared.save(stringValue, forKey: thirdKey, toPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Value '\(stringValue)' successfully saved at Key '\(thirdKey)' into '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.save(stringValue, forKey: thirdKey, toPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Value '\(stringValue)' successfully saved at Key '\(thirdKey)' into '\(dataPlistName).plist'")
+      }
+    }*/
     
     ////////////////////////////////////////////////////////////////////////////////////
     // Better change back the value to a Bool for the Item with key 'thirdKey' before you forget it.
     
     /*
-     SwiftyPlistManager.shared.save(boolValue, forKey: thirdKey, toPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Value '\(boolValue)' successfully saved at Key '\(thirdKey)' into '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.save(boolValue, forKey: thirdKey, toPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Value '\(boolValue)' successfully saved at Key '\(thirdKey)' into '\(dataPlistName).plist'")
+      }
+    }*/
     
     // The warning will come up this time too, but now you know that it is set back the way you need it.
     
@@ -335,22 +334,22 @@ class ViewController: UIViewController {
     // by adding a new dictionary.
     
     /*
-     SwiftyPlistManager.shared.addNew(dict, key: fourthKey, toPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Value '\(dict)' successfully saved at Key '\(fourthKey)' into '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.addNew(dict, key: fourthKey, toPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Value '\(dict)' successfully saved at Key '\(fourthKey)' into '\(dataPlistName).plist'")
+      }
+    }*/
     
     ////////////////////////////////////////////////////////////////////////////////////
     // Now just to have some fun with it, change the age of John to 56.
     
     /*
-     dict["age"] = 56
-     SwiftyPlistManager.shared.save(dict, forKey: fourthKey, toPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Value '\(dict)' successfully saved at Key '\(fourthKey)' into '\(dataPlistName).plist'")
-     }
-     }*/
+    dict["age"] = 56
+    SwiftyPlistManager.shared.save(dict, forKey: fourthKey, toPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Value '\(dict)' successfully saved at Key '\(fourthKey)' into '\(dataPlistName).plist'")
+      }
+    }*/
     
     // Well done! Now comment back the calls.
     
@@ -358,11 +357,11 @@ class ViewController: UIViewController {
     // Once in a while you might want to remove a value-key pair
     
     /*
-     SwiftyPlistManager.shared.removeValueKeyPair(for: thirdKey, fromPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Key-Value pair successfully removed at Key '\(thirdKey)' from '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.removeValueKeyPair(for: thirdKey, fromPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Key-Value pair successfully removed at Key '\(thirdKey)' from '\(dataPlistName).plist'")
+      }
+    }*/
     
     // Of course, if this line is executed several times 'SwiftyPlistManager' realises that the item
     // was already removed and does not exist. Try it out!
@@ -371,11 +370,11 @@ class ViewController: UIViewController {
     // Or you might want to delete all the data from your plist file
     
     /*
-     SwiftyPlistManager.shared.removeAllKeyValuePairs(fromPlistWithName: dataPlistName) { (err) in
-     if err == nil {
-     print("-------------> Successfully removed all Key-Value pairs from '\(dataPlistName).plist'")
-     }
-     }*/
+    SwiftyPlistManager.shared.removeAllKeyValuePairs(fromPlistWithName: dataPlistName) { (err) in
+      if err == nil {
+        print("-------------> Successfully removed all Key-Value pairs from '\(dataPlistName).plist'")
+      }
+    }*/
     
     // Of course, if this line is executed several times 'SwiftyPlistManager' realises that the plist
     // is empty and cancels the operation. Try it out.
@@ -396,12 +395,12 @@ class ViewController: UIViewController {
     // but you can do whatever you want.
     
     /*
-     func logSwiftyPlistManager(_ error: SwiftyPlistManagerError?) {
-     guard let err = error else {
-     return
-     }
-     print("-------------> SwiftyPlistManager error: '\(err)'")
-     }*/
+    func logSwiftyPlistManager(_ error: SwiftyPlistManagerError?) {
+      guard let err = error else {
+        return
+      }
+      print("-------------> SwiftyPlistManager error: '\(err)'")
+    }*/
     
     ////////////////////////////////////////////////////////////////////////////////////
     // Now let's take a look at the most common use cases and their errors
@@ -409,49 +408,49 @@ class ViewController: UIViewController {
     // fileUnavailable
     
     /*
-     SwiftyPlistManager.shared.addNew(helloNewValue, key: newKey, toPlistWithName: nonExistentPlistName) { (err) in
-     if err != nil {
-     logSwiftyPlistManager(err)
-     }
-     }*/
+    SwiftyPlistManager.shared.addNew(helloNewValue, key: newKey, toPlistWithName: nonExistentPlistName) { (err) in
+      if err != nil {
+        logSwiftyPlistManager(err)
+      }
+    }*/
     
     ////////////////////////////////////////////////////////////////////////////////////
     // fileAlreadyEmpty
     
     /*
-     SwiftyPlistManager.shared.removeAllKeyValuePairs(fromPlistWithName: dataPlistName) { (err) in
-     if err != nil {
-     logSwiftyPlistManager(err)
-     }
-     }*/
+    SwiftyPlistManager.shared.removeAllKeyValuePairs(fromPlistWithName: dataPlistName) { (err) in
+      if err != nil {
+        logSwiftyPlistManager(err)
+      }
+    }*/
     
     ////////////////////////////////////////////////////////////////////////////////////
     // keyValuePairAlreadyExists
     
     /*
-     SwiftyPlistManager.shared.addNew(rebeloperValue, key: firstKey, toPlistWithName: dataPlistName) { (err) in
-     if err != nil {
-     logSwiftyPlistManager(err)
-     } else {
-     print("-------------> Value '\(rebeloperValue)' successfully added at Key '\(firstKey)' into '\(dataPlistName).plist'")
-     }
-     }
-     
-     SwiftyPlistManager.shared.addNew(rebeloperValue, key: firstKey, toPlistWithName: dataPlistName) { (err) in
-     if err != nil {
-     logSwiftyPlistManager(err)
-     }
-     }*/
+    SwiftyPlistManager.shared.addNew(rebeloperValue, key: firstKey, toPlistWithName: dataPlistName) { (err) in
+      if err != nil {
+        logSwiftyPlistManager(err)
+      } else {
+        print("-------------> Value '\(rebeloperValue)' successfully added at Key '\(firstKey)' into '\(dataPlistName).plist'")
+      }
+    }
+    
+    SwiftyPlistManager.shared.addNew(rebeloperValue, key: firstKey, toPlistWithName: dataPlistName) { (err) in
+      if err != nil {
+        logSwiftyPlistManager(err)
+      }
+    }*/
     
     ////////////////////////////////////////////////////////////////////////////////////
     // keyValuePairDoesNotExist
     
     /*
-     SwiftyPlistManager.shared.getValue(for: nonExistentKey, fromPlistWithName: dataPlistName) { (result, err) in
-     if err != nil {
-     logSwiftyPlistManager(err)
-     }
-     }*/
+    SwiftyPlistManager.shared.getValue(for: nonExistentKey, fromPlistWithName: dataPlistName) { (result, err) in
+      if err != nil {
+        logSwiftyPlistManager(err)
+      }
+    }*/
     
     ////////////////////////////////////////////////////////////////////////////////////
     
